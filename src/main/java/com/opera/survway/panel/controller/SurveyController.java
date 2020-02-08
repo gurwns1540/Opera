9a0c2fda6edce0f4ab4e39cbd14270d34652ca99
Sooo <Sooo@DESKTOP-8NVFIQH>
2020-02-09 08:15:06 +0900

    응답 기록 작업중

--- src/main/java/com/opera/survway/panel/controller/SurveyController.java
+++ src/main/java/com/opera/survway/panel/controller/SurveyController.java
@@ -33,15 +33,13 @@ public class SurveyController {
 	 * @Description : 패널 정보에 맞는 설문조사 목록 조회 - ts조사(신규패널)는 바로 questions&choices 조회
 	 */
 	@RequestMapping("surveyList.survey")
-	public String selectNotice(HttpServletRequest request, Model model) {
+	public String selectSurveyList(HttpServletRequest request, Model model) {
 		
 		//request에서 패널정보 받아오기
 		PanelMember loginUser = (PanelMember)request.getSession().getAttribute("loginUser");
 		
 		//뷰페이지로 보낼 변수들 선언
 		List<Research> researchList = null;
-		List<ResearchQuestion> researchQuestionList = null;
-		int qCount = 0;
 		String msg = "noMessage";
 		//리턴경로
 		String path = "";
@@ -59,65 +57,47 @@ public class SurveyController {
 			}
 		}
 		
-		try {
+		switch(loginUser.getPanellevelNo()) {
+		
+		//신규회원일때 - 리서치정보조회 X, TS조사 질문들 바로 리턴
+		case 1 : 
+			path = "surveyList";
+			break;
 			
-			switch(loginUser.getPanellevelNo()) {
-			
-			//신규회원일때 - 리서치정보조회 X, TS조사 질문들 바로 리턴
-			case 1 : 
-				List<ResearchQuestion> tsQuestions = ps.getTsQuestionList();
-				researchQuestionList = tsQuestions;
-				qCount = tsQuestions.size();
-				for(ResearchQuestion r : tsQuestions) {
-					r.setProgressDataPercent((int)(Math.round((r.getResearchOrder()-1)/((double)qCount)*100)));
-				}
-				path = "tsSurveyList";
-				break;
-				
-			//일반 회원일때 (비활성.준활성.활성) - 설문조사 목록 구성용 조회 (각 설문조사 진행은 따로 ajax처리)
-			case 2 : case 3 : case 4 :
-				listCount = ps.getCountMyResearch(loginUser);
-				pi = Pagination.getPageInfo(currentPage, listCount);
-				List<Research> myResearches = ps.getMyResearchList(loginUser, pi);
-				researchList = myResearches;
-				if(listCount == 0) {
-					msg = "현재 참여 가능한 설문조사가 없습니다.";
-				}
-				path = "surveyList";
-				break;
-				
-			//휴면회원일때
-			case 5 :
-				msg = "SURVWAY를 오랫동안 이용하지 않아 회원님의 아이디가 휴면 상태로 전환되었습니다.<br>회원정보 재인증 후 이용하실 수 있습니다.";
-				path = "surveyList";
-				break;
-				
-			//블랙회원일때
-			case 6 :
-				msg = "회원님께서는 3회 불량 응답하였으므로 설문조사에 참여하실 수 없습니다.";
-				path = "surveyList";
-				break;
-			
-			default : return "";
-			
+		//일반 회원일때 (비활성.준활성.활성) - 설문조사 목록 구성용 조회 (각 설문조사 진행은 따로 ajax처리)
+		case 2 : case 3 : case 4 :
+			listCount = ps.getCountMyResearch(loginUser);
+			pi = Pagination.getPageInfo(currentPage, listCount);
+			List<Research> myResearches = ps.getMyResearchList(loginUser, pi);
+			researchList = myResearches;
+			if(listCount == 0) {
+				msg = "현재 참여 가능한 설문조사가 없습니다.";
 			}
+			path = "surveyList";
+			break;
 			
+		//휴면회원일때
+		case 5 :
+			msg = "SURVWAY를 오랫동안 이용하지 않아 회원님의 아이디가 휴면 상태로 전환되었습니다.<br>회원정보 재인증 후 이용하실 수 있습니다.";
+			path = "surveyList";
+			break;
 			
-		} catch (SelectException e) {
-			e.printStackTrace();
-			return "";
+		//블랙회원일때
+		case 6 :
+			msg = "회원님께서는 3회 불량 응답하였으므로 설문조사에 참여하실 수 없습니다.";
+			path = "surveyList";
+			break;
+		
+		default : return "";
+		
 		}
 		
 		
-		//신규회원용
-		model.addAttribute("researchQuestionList", researchQuestionList);
-		model.addAttribute("qCount", qCount);
-		//일반회원용
+		//신규 및 일반회원용
 		model.addAttribute("researchList", researchList);
+		model.addAttribute("pi", pi);
 		//휴면or블랙리스트회원용
 		model.addAttribute("msg", msg);
-		//페이징용(공통)
-		model.addAttribute("pi", pi);
 		
 		return path;
 	}
@@ -129,7 +109,7 @@ public class SurveyController {
 	 * @Description : 설문조사 목록 중 선택한 설문조사에 대한 문제 및 보기 리스트 조회
 	 */
 	@PostMapping("selectResearchQuestions.survey")
-	public ModelAndView selectNewPanel(HttpServletRequest request, String reward, String researchNo, String userName, String panellevelNo, ModelAndView mv) {
+	public ModelAndView selectResearchQuestionList(String reward, String researchNo, String userName, String panellevelNo, ModelAndView mv) {
 		
 		//문항수
 		int qCount = 0;
@@ -186,5 +166,21 @@ public class SurveyController {
 		
 		return mv;
 	}
+	
+	@PostMapping("insertResearchAnswers.survey")
+	public ModelAndView insertResearchAnswerList(String totalAnswer, ModelAndView mv) {
+		
+		System.out.println("totalAnswer : " + totalAnswer);
+		
+		
+		
+		mv.setViewName("jsonView");
+		return mv;
+	}
+	
+	
+	
+	
+	
 	
 }
