0663faafc41ae26bea5f3a4f58b2b2c8ca7e12dd
im233 <im233@DESKTOP-BN4GTCQ>
2020-02-11 20:24:11 +0900

    thanksSurvey 승인 또는 반려 메일링과 panelLevelUp or researchdelete

--- src/main/java/com/opera/survway/admin/model/dao/AdminDaoImpl.java
+++ src/main/java/com/opera/survway/admin/model/dao/AdminDaoImpl.java
@@ -402,4 +402,16 @@ public class AdminDaoImpl implements AdminDao{
 		return sqlSession.selectOne("Admin.selectResearchPeriod", researchNo);
 	}
 
+	@Override
+	public int updatePanelLevel(SqlSessionTemplate sqlSession, int mno) {
+		
+		return sqlSession.update("Admin.updatePanelLevel",mno);
+	}
+
+	@Override
+	public int deletePanelThanksSurvey(SqlSessionTemplate sqlSession, int mno) {
+		
+		return sqlSession.delete("Admin.deletePanelThanksSurvey",mno);
+	}
+
 }
